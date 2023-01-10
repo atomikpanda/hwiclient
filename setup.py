93a9a28677251424fa5e8473ce80ecf7a6643fcb
Bailey Seymour <baileyseymour@protonmail.com>
2023-01-10 12:46:40 -0500

    version num

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup(name='hwiclient', version='1.0.0', packages=['hwiclient', 'hwiclient.commands', 'hwiclient.connection'],
+setup(name='hwiclient', version='1.0.1', packages=['hwiclient', 'hwiclient.commands', 'hwiclient.connection'],
       install_requires=[
 
 
